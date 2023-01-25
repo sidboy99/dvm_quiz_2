69ba53d9523cc2f48ab85b8ac877f727c9cf528f
sidboy99 <62690924+sidboy99@users.noreply.github.com>
2023-01-26 03:29:07 +0530

    Add files via upload

--- users/forms.py
+++ users/forms.py
@@ -3,13 +3,30 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
-
+'''
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+'''
+
+
+class UserRegisterForm(UserCreationForm):
+    user_type = forms.ChoiceField(choices=Profile.choices)
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username','email','password1', 'password2', 'user_type']
+
+    def save(self, *args, **kwargs):
+        user = super().save(*args, **kwargs)
+        profile = Profile.objects.get(user=user)
+        profile.user_type = self.cleaned_data['user_type']
+        profile.save()
+        return profile
 
 
 class UserUpdateForm(forms.ModelForm):
@@ -23,4 +40,4 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['user_type','image']
+        fields = ['image']
