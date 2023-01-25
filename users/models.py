6f1b17c2e2f87d4bb7767390a6b9ca5decdc239e
sidboy99 <62690924+sidboy99@users.noreply.github.com>
2023-01-25 23:35:24 +0530

    Add files via upload

--- users/models.py
+++ users/models.py
@@ -4,19 +4,19 @@ from PIL import Image
 
 
 class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     choices = [
         ('QM', 'QM'),
         ('QT', 'QT'),
     ]
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     user_type = models.CharField(default='QT', choices=choices, max_length=2)
 
     def __str__(self):
         return f'{self.user.username} {self.user_type} Profile'
 
-    def save(self):
-        super().save() # runs the save method from the parent class
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs) # runs the save method from the parent class
 
         img = Image.open(self.image.path)
 
